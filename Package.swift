591813c1485ba756276a3d70140706c035c2cc5d
Russell Warwick <russell.warwick@gymshark.com>
2021-05-07 22:36:17 +0100

    Adds SharkUtils package

--- Package.swift
+++ Package.swift
@@ -7,21 +7,20 @@ let package = Package(
     name: "SharkStackKit",
     platforms: [ .iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SharkStackKit",
             targets: ["SharkStackKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            name: "SharkUtils",
+            url: "git@github.com:gymshark/ios-Shark-Utils.git",
+            .upToNextMinor(from: "1.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SharkStackKit",
-            dependencies: []),
+            dependencies: ["SharkUtils"]),
         .testTarget(
             name: "SharkStackKitTests",
             dependencies: ["SharkStackKit"]),
